e18d205fe5911677aab2d08c46203614dcf93732
mawenhao <mawh@bxlsj.com>
2023-09-26 23:01:13 +0800

    draft: 构建AST

--- simple-parser/src/main/java/cn/yangtuooc/Main.java
+++ simple-parser/src/main/java/cn/yangtuooc/Main.java
@@ -6,6 +6,19 @@ import cn.yangtuooc.parser.SimpleParser;
 import cn.yangtuooc.reader.CharacterReader;
 import cn.yangtuooc.token.TokenStream;
 
+/**
+ * 支持语法规则：
+ *
+ * expr -> add | sub | mul | div
+ * add -> add + expr | primitive
+ * sub -> sub - expr | primitive
+ * mul -> mul * expr | primitive
+ * div -> div / expr | primitive
+ * primitive -> [0-9]+
+ *
+ * fixme: 这个文法规则会有结合性问题：2+3+5，应该先计算2+3，而上述文法是先计算3+5
+ *
+ */
 public class Main {
 
   public static void main(String[] args) {
